a3c12f9bd8acdd9d37596f019454bbc18eb59972
ThulaniTheProgrammer <frankmakeba0@gmail.com>
2024-08-16 11:48:14 +0200

    Celebration

--- src/pages/Terms.jsx
+++ src/pages/Terms.jsx
@@ -7,7 +7,7 @@ export default function Terms() {
                 <div className="max-w-md w-full bg-white p-8 shadow-md rounded-md">
                     <h2 className="text-2xl font-bold mb-4">Celebration Fun Run Registration Waiver</h2>
                     <p className="mb-4">
-                    I, hereby acknowledge and agree that participation in the Celebration  fun run is entirely voluntary. I understand that there are inherent risks associated with running a marathon, including but not limited to physical injury, property damage, and any unforeseen loss.
+                    I, hereby acknowledge and agree that participation in the Celebration  fun run is entirely voluntary. I understand that there are inherent risks associated with running in this event, including but not limited to physical injury, property damage, and any unforeseen loss.
                     </p>
                     <p className="mb-4">
                     I assume full responsibility for any and all injuries, damages, or losses that I may suffer as a result of my participation in the fun run. I release, discharge, and hold harmless Celebration , its officers, directors, employees, volunteers, and agents from any and all claims, demands, actions, or causes of action arising out of or in connection with my participation in the fun run.
