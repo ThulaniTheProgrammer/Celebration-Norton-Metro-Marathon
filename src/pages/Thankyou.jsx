8720f720e83561665a2f44071cddf0c266393b14
ThulaniTheProgrammer <frankmakeba0@gmail.com>
2024-08-16 10:14:03 +0200

    Updated

--- src/pages/Thankyou.jsx
+++ src/pages/Thankyou.jsx
@@ -6,6 +6,7 @@ export default function Thankyou() {
             <div className="bg-white p-8 rounded shadow">
                 <h1 className="text-3xl font-bold mb-4">Thank You!</h1>
                 <p className="text-gray-700">We appreciate your support.</p>
+                <p className="text-gray-700">For more information contact 0774521028</p>
             </div>
         </div>
     )
