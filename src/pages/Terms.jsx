dbadaba79aac8c28a94fb9057adf3c197bc43a7c
ThulaniTheProgrammer <frankmakeba0@gmail.com>
2024-08-16 11:05:10 +0200

    Updated

--- src/pages/Terms.jsx
+++ src/pages/Terms.jsx
@@ -7,10 +7,10 @@ export default function Terms() {
                 <div className="max-w-md w-full bg-white p-8 shadow-md rounded-md">
                     <h2 className="text-2xl font-bold mb-4">Fun Run Registration Waiver</h2>
                     <p className="mb-4">
-                    I, hereby acknowledge and agree that participation in the Celebration Church Norton Metro marathon is entirely voluntary. I understand that there are inherent risks associated with running a marathon, including but not limited to physical injury, property damage, and any unforeseen loss.
+                    I, hereby acknowledge and agree that participation in the Celebration Church Norton Metro fun run is entirely voluntary. I understand that there are inherent risks associated with running a marathon, including but not limited to physical injury, property damage, and any unforeseen loss.
                     </p>
                     <p className="mb-4">
-                    I assume full responsibility for any and all injuries, damages, or losses that I may suffer as a result of my participation in the marathon. I release, discharge, and hold harmless Celebration Church Norton Metro, its officers, directors, employees, volunteers, and agents from any and all claims, demands, actions, or causes of action arising out of or in connection with my participation in the marathon.
+                    I assume full responsibility for any and all injuries, damages, or losses that I may suffer as a result of my participation in the marathon. I release, discharge, and hold harmless Celebration Church Norton Metro, its officers, directors, employees, volunteers, and agents from any and all claims, demands, actions, or causes of action arising out of or in connection with my participation in the fun run.
                     </p>
                     <p className="mb-4">
                     By signing this waiver, I certify that I am physically fit to participate in the marathon and have not been advised by a medical professional to refrain from such activity
