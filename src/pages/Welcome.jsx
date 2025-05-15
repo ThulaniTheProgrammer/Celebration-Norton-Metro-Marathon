9208470afe53cae35da173a6a595ae7d59511006
ThulaniTheProgrammer <frankmakeba0@gmail.com>
2025-05-15 14:52:29 +0200

    2025 version

--- src/pages/Welcome.jsx
+++ src/pages/Welcome.jsx
@@ -4,8 +4,8 @@ import Typewriter from 'typewriter-effect';
 const Welcome = () => {
   return (
     
-    <div className='h-screen w-screen flex flex-col bg-no-repeat bg-cover' style={{ backgroundImage: "url('https://bywjfszqwytcvsftccvd.supabase.co/storage/v1/object/public/Marathon/marathon-6660178_1280.jpg?t=2024-07-29T09%3A09%3A01.933Z')", filter: 'brightness(100%)' }}>
-      <img src="https://bywjfszqwytcvsftccvd.supabase.co/storage/v1/object/public/Marathon/download__1_-removebg-preview.png?t=2024-07-29T20%3A41%3A58.096Z" alt="Logo" className="w-20 h-10" />
+    <div className='h-screen w-screen flex flex-col bg-no-repeat bg-cover h-full' style={{ backgroundImage: "url('https://aqlztcsukugmsztrrkau.supabase.co/storage/v1/object/sign/buck/DSC05443.jpg?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6InN0b3JhZ2UtdXJsLXNpZ25pbmcta2V5X2M3NGYxODUyLTExNWMtNGFhYy04MmEzLWNkZDc0ZDg0YzY1NiJ9.eyJ1cmwiOiJidWNrL0RTQzA1NDQzLmpwZyIsImlhdCI6MTc0NzMxMTk5NSwiZXhwIjoxNzc4ODQ3OTk1fQ.9jwRJ6o_59cmr_SYzTucDj0LzNy3SQ-yH7B3GsZVp_M')", filter: 'brightness(100%)' }}>
+      <img src="https://celebrate.org/wp-content/uploads/2023/12/Celebration-Ministires-Horizontal-Black.png" alt="Logo" className="w-40 h-20" />
       <div className='text-center flex justify-center items-center opacity-800 text-white text-4xl font-extrabold opacity-800 mt-40'>
       <Typewriter
           options={{
@@ -16,15 +16,15 @@ const Welcome = () => {
           }}
         />
       </div>
-      <div className='text-center flex justify-center items-center opacity-800 text-white lg:text-4xl text:2xl  font-extrabold opacity-800 '>Celebration Fun Run</div>
-      <div className='text-center flex justify-center items-center opacity-800 text-white text-4xl text:2xl font-extrabold opacity-800 '>2024 Fun Run Registration Platform</div>
+      <div className='text-center flex justify-center items-center opacity-800 text-white tex  font-extrabold opacity-800 '>Celebration Fun Run</div>
+      <div className='text-center flex justify-center items-center opacity-800 text-white text-4xl text:2xl font-extrabold opacity-800 '>2025 Fun Run Registration Platform</div>
     
       <div className='text-center flex justify-center items-center opacity-800 text-gray-600 text-sm font-extrabold opacity-800 mt-20 '> 
         <a href="/Register" className='text-white text-center text-xl font-semibold  flex justify-center items-center  w-45 bg-blue-800 px-4 py-4 rounded-xl lg:mr-20'>Register As Runner</a>
 
 
 
-        <div  className='text-white text-center  text-xl font-semibold  justify-center items-center   w-45 bg-slate-700 px-4 py-4 hidden rounded-xl ml-20'>Register As Sponsor</div>
+        <div  className='text-white text-center  text-xl font-semibold  justify-center items-center   w-45 bg-slate-700 px-4 py-4  hidden rounded-xl ml-20'>Register As Sponsor</div>
 
 
 
@@ -32,7 +32,7 @@ const Welcome = () => {
 
       
     
-      <div className=" text-white mb-0 h-full mt-10 flex flex-col lg:flex-row justify-between bg-blue-800 p-8 rounded shadow">
+      <div className=" text-white mb-0 h-full mt-40 flex flex-col lg:flex-row justify-between bg-blue-800 p-8 rounded shadow">
         
                 
                 <div>
